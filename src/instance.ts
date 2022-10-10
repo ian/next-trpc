2e240123b2c820a844733e1a0db14a928dd8ae20
Ian Hunter <ian@users.noreply.github.com>
2022-10-10 10:57:13 -0700

    cleanup

--- src/instance.ts
+++ src/instance.ts
@@ -1,7 +1,6 @@
 import { initTRPC } from "@trpc/server"
 
 export const trpc = initTRPC.context().create({
-  // transformer: superjson,
   errorFormatter({ error, shape }) {
     console.error(error)
     return shape
