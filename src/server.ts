cf2fa8bd5d755a98c2586b6310b0aa636422ea06
Ian Hunter <ian@users.noreply.github.com>
2023-04-25 07:33:27 -0500

    send all opts not just two

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,7 @@ import * as trpcNext from "@trpc/server/adapters/next"
 type Opts = Parameters<typeof trpcNext.createNextApiHandler>[0]
 
 export function nextTRPC(opts: Opts) {
-  const { router, createContext } = opts
-  return trpcNext.createNextApiHandler({
-    router,
-    createContext
-  })
+  return trpcNext.createNextApiHandler(opts)
 }
 
 export { TRPCError } from "@trpc/server"
